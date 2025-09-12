e5b4b82dc72ff13324d365f5ad94f2e43c8f3222
zili <neodejack@gmail.com>
2025-09-12 18:46:05 +0800

    dbg()

--- lib/bupe/parser.ex
+++ lib/bupe/parser.ex
@@ -265,7 +265,7 @@ defmodule BUPE.Parser do
   defp find_manifest(xml, media_type), do: find_manifest(xml, [media_type])
 
   defp find_xml(xml, filter: filter, type: :attribute),
-    do: filter |> xpath_string(xml) |> transform()
+    do: filter |> xpath_string(xml) |> transform() |> dbg()
 
   defp find_xml(xml, filter: filter, type: type),
     do: filter |> xpath_string(xml) |> transform(from: type)
